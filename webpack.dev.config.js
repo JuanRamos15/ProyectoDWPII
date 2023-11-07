a4454430002e1b4e615d012bf0d3058a009b1ecc
JuanRamos15 <124464419+JuanRamos15@users.noreply.github.com>
2023-11-07 22:48:31 +0000

    🌟Enhance: Webpack Front-end webpack and frontend babel

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -1,9 +1,7 @@
 // Notas importanes
 // El archivo de configuración debe usar ES5
-
 // Importar un administrador de rutas de archivos
 const path = require("path");
-
 // Exportamos un objeto de configuración
 // que sera usado por webpack
 module.exports = {
