2fd583ed7017177284b2611191596c5b69120097
JuanRamos15 <124464419+JuanRamos15@users.noreply.github.com>
2023-11-10 22:36:58 +0000

    ⚙BUGFIX: Added css-loader, mini-css-extract-plugin

---
+++ public/bundle.js
@@ -0,0 +1,1 @@
+console.log("Webpack Working!!! 📦"),function(){let o=arguments.length>0&&void 0!==arguments[0]?arguments[0]:"No message given";console.log(o)}(),async function(){console.log("Calling");const o=await new Promise((o=>{setTimeout((()=>{o("resolved")}),2e3)}));console.log(o)}();
