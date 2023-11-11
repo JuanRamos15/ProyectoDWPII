1415579f36af7c40abe96939ac58f7a778943a63
David Israel <118939519+david-Israel26@users.noreply.github.com>
2023-11-11 17:19:02 +0000

    🌠Enhance: Webpack Middleware Some configurations were made into the project + implementation of a debug logger.

--- server/app.js
+++ server/app.js
@@ -4,12 +4,50 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var debug = require('debug')('dwpc2:server');
+//var debug = require('debug')('dwpc2:server');
+
+//Importing debug into app.js file
+import debug from './services/degubLogger';
+
+//Establishing webpack modules
+import webpack from 'webpack';
+import WebpackDevMiddleware from 'webpack-dev-middleware';
+import WebpackHotMiddleware from 'webpack-hot-middleware';
+//Importing the configuration of the webpack module
+import webpackConfig from '../webpack.dev.config';
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 //Creando la instancia de express
 var app = express();
+
+//Establishing a execution mode
+const nodeEnviroment = process.env.NODE_ENV || 'production';
+if(nodeEnviroment === 'development'){
+  // Mensaje de que se comienza a ejecutar el modo de desarrollo
+  debug("Ejecutando en modo desarrollo 🛠️");
+  // Estableciendo el entorno de node al modo de webpack
+  webpackConfig.mode = nodeEnviroment;
+  // Emparejando el puerto de webpack con el de node
+  webpackConfig.devServer.port = process.env.PORT;
+  // Setting up the HMR (Hot Module Replacement)
+  webpackConfig.entry = [
+    "webpack-hot-middleware/client?reload=true&timeout=1000",
+    webpackConfig.entry
+  ];
+	// Se agrega el plugin de la configuracion de desarrollo de webpack
+  webpackConfig.plugins.push(new webpack.HotModuleReplacementPlugin());
+  // Se crea el archivo bundle y se guarda en una constante
+  const bundle = webpack(webpackConfig);
+  // Permitiendo el uso del middleware
+  app.use( WebpackDevMiddleware(bundle, {
+    publicPath: webpackConfig.output.publicPath
+  }) );
+  //  Enabling the webpack HMR
+  app.use( WebpackHotMiddleware(bundle) );
+}else{
+  console.log("Ejecutando en modo producción 🪖");
+}
 
 // configura el motor de plantillas
 debug(`📣Ruta de app: ${path.join(__dirname,'views')}`);
---
+++ server/services/degubLogger.js
@@ -0,0 +1,6 @@
+//Importing debug dependency
+import Debug from 'debug';
+//Establishing the process to debug
+const debug = Debug('dwpc2:server');
+//Exporting the debug logger
+export default debug;
