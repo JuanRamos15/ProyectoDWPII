a1114d2518206230a07847255b9118982026b5ab
David Israel <118939519+david-Israel26@users.noreply.github.com>
2023-11-11 18:38:23 +0000

    Feature: Prettier & Eslint Added Prettier and Eslint to the project

--- client/index.js
+++ client/index.js
@@ -1,30 +1,6 @@
-
 // cargando los estilos
-import './styles/style.css'
-import './styles/style-b.css'
-console.log("🎉 Estilos cargados correctamente 🎉");
+import './styles/style.css';
 
+import './styles/style-b.css';
 
-// Codigo ES6
-// Default Parameters
-let show = (msg="No message given") => {
-  console.log(msg)
-}
-show();
-
-// Async Await
-function resolveAfter2Seconds() {
-  return new Promise(resolve => {
-    setTimeout(() => {
-      resolve('resolved');
-    }, 2000)
-  });
-}
-
-async function asyncCall() {
-  console.log("Calling");
-  const result = await resolveAfter2Seconds();
-  console.log(result);
-}
-
-asyncCall();
+console.log('🎉 Estilos cargados correctamente 🎉');
