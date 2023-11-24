844837fe8923f72c01f8936741ef76e53138a178
JuanRamos15 <124464419+JuanRamos15@users.noreply.github.com>
2023-11-24 20:37:32 +0000

    🌟Enhance Data Persistence: Added front-end validation and added database data persistence

--- server/domains/projects/project.controller.js
+++ server/domains/projects/project.controller.js
@@ -1,6 +1,8 @@
 // Metodos
-// actions methods
+// actions methods, importing winston logger
 import log from '../../config/winston';
+// Importando el modelo
+import ProjectModel from './project.model';
 // get 'project/projects'
 const showDashboard = (req, res) => {
   res.render('project/addView');
@@ -11,7 +13,7 @@ const addForm = (req, res) => {
   res.render('project/addView');
 };
 
-const addPost = (req, res) => {
+const addPost = async (req, res) => {
   // Rescatando la info del formulario
   const { errorData: validationError } = req;
   // En caso de haber error
@@ -34,10 +36,18 @@ const addPost = (req, res) => {
   // Se desestructura la información
   // de la peticion
   const { validData: project } = req;
-  // Se contesta la información
-  // del proyecto al cliente
-  log.info('Se entrega al cliente información del proyecto cargado');
-  return res.status(200).json(project);
+  try {
+    // Creando la instancia de un documento con los valores de 'project'
+    const savedProject = await ProjectModel.create(project);
+    // Se contesta la información del proyecto al cliente
+    log.info('Se entrega al cliente información del proyecto cargado');
+    return res.status(200).json(savedProject);
+  } catch (error) {
+    log.error(
+      'ln 53 project.controller: Error al guardar proyecto en la base de datos'
+    );
+    return res.status(500).json(error);
+  }
 };
 
 // Controlador Home
