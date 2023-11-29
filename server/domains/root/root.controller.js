84557590063a1505d76bb7c4fb0be3161cf9c751
JuanRamos15 <124464419+JuanRamos15@users.noreply.github.com>
2023-11-29 23:57:13 +0000

    ✴Feature rootViews: Added root views for Home, Add Books, List Books

--- server/domains/root/root.controller.js
+++ server/domains/root/root.controller.js
@@ -1,7 +1,21 @@
+import log from '../../config/winston';
+// import root from './book.model';
 const rootNav = (request, response) => {
-  response.render('root/navbarRoot');
+  response.render('root/rootHome');
+};
+// GET '/root/register'
+const addBook = (req, res) => {
+  log.info('Se entrega formulario de registro de libro');
+  res.render('root/addBook');
+};
+// GET '/root/register'
+const listBooks = (req, res) => {
+  log.info('Se entrega la lista de libros registrados en el sistema');
+  res.render('root/listBooks');
 };
 
 export default {
   rootNav,
+  addBook,
+  listBooks,
 };
--- server/domains/root/root.router.js
+++ server/domains/root/root.router.js
@@ -3,7 +3,11 @@ import { Router } from 'express';
 import rootController from './root.controller';
 
 const router = new Router();
-
-router.get('/', rootController.rootNav);
+// GET '/root/rootHome'
+router.get('/rootHome', rootController.rootNav);
+// GET '/root/addBook'
+router.get('/addBook', rootController.addBook);
+// GET '/root/listBooks'
+router.get('/listBooks', rootController.listBooks);
 
 export default router;
